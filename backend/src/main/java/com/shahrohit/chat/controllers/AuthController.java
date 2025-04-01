9c2f24172f984f421ed49bd0585e8f6dfdd31984
shahrohit <shahrohit@example.com>
2025-04-01 14:07:44 +0530

    updated controllers

--- backend/src/main/java/com/shahrohit/chat/controllers/AuthController.java
+++ backend/src/main/java/com/shahrohit/chat/controllers/AuthController.java
@@ -1,5 +1,7 @@
 package com.shahrohit.chat.controllers;
 
+import com.shahrohit.chat.dtos.AuthResponse;
+import com.shahrohit.chat.dtos.LoginRequest;
 import com.shahrohit.chat.dtos.RegisterRequest;
 import com.shahrohit.chat.dtos.VerifyOtpRequest;
 import com.shahrohit.chat.services.AuthService;
@@ -15,12 +17,22 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public String registerUser(@Valid @RequestBody RegisterRequest body){
+    public AuthResponse registerUser(@Valid @RequestBody RegisterRequest body){
         return authService.registerUser(body);
     }
 
     @PostMapping("/verify-otp")
-    public String verifyOtp(@Valid @RequestBody VerifyOtpRequest body){
-        return authService.verifyOtp(body.getUsername(), body.getOtp());
+    public AuthResponse verifyOtp(@Valid @RequestBody VerifyOtpRequest body){
+        return authService.verifyOtp(body);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@Valid @RequestBody LoginRequest body){
+        return authService.login(body);
+    }
+
+    @PostMapping("/verify-device")
+    public AuthResponse verifyDevice(@Valid @RequestBody VerifyOtpRequest body){
+        return authService.verifyNewDevice(body);
     }
 }
