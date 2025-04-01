3da767bee724679b276af1b8815e9f1aad7b653a
shahrohit <shahrohit@example.com>
2025-04-01 14:04:12 +0530

    updated adapters

--- backend/src/main/java/com/shahrohit/chat/adapters/UserAdapter.java
+++ backend/src/main/java/com/shahrohit/chat/adapters/UserAdapter.java
@@ -1,18 +1,12 @@
 package com.shahrohit.chat.adapters;
 
 import com.shahrohit.chat.dtos.RegisterRequest;
-import com.shahrohit.chat.models.Role;
+import com.shahrohit.chat.dtos.UserDto;
 import com.shahrohit.chat.models.User;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserAdapter {
-    private final PasswordEncoder passwordEncoder;
-
-    private UserAdapter(PasswordEncoder passwordEncoder){
-        this.passwordEncoder = passwordEncoder;
-    }
 
     public User toUser(RegisterRequest request){
         return User.builder()
@@ -21,9 +15,19 @@ public class UserAdapter {
             .about(request.getAbout())
             .username(request.getUsername())
             .email(request.getEmail())
-            .password(passwordEncoder.encode(request.getPassword()))
-            .role(Role.USER)
+            .password(request.getPassword())
             .enabled(false)
+            .build();
+    }
+
+    public UserDto toUserDto(User user){
+        return UserDto.builder()
+            .firstName(user.getFirstName())
+            .lastName(user.getLastName())
+            .about(user.getAbout())
+            .email(user.getEmail())
+            .username(user.getUsername())
+            .profilePictureUrl(user.getProfilePictureUrl())
             .build();
     }
 }
